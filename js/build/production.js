8e8597b590829863f88671be1450c186b8534a32
Alberto Gorgonio <gorgonioalberto@gmail.com>
2017-07-20 10:14:33 +0200

    ...

--- js/build/production.js
+++ js/build/production.js
@@ -409,6 +409,7 @@ function initPages(page) {
         var scrollArea = page.querySelector('.vertical_align');
         var projects = page.querySelectorAll('.project');
         var filterCount = page.querySelector('.count');
+        var projectsLoaded = false;
         var infProject, loadAll;
 
         setTimeout(function() {
@@ -499,7 +500,7 @@ function initPages(page) {
             clearInterval(loadAll);
 
             var updateProject = page.querySelectorAll('.project');
-            
+
             if (filterValue !== '*') {
 
                 forEach(updateProject, function (index, elem) {
@@ -534,14 +535,20 @@ function initPages(page) {
 
         function preloadAllPages(filterValue) {
 
-            loadAll = setInterval(function(){
-                if (infProject.loadCount !== 2) {
-                    infProject.loadNextPage();
-                } else {
-                    filterProject(filterValue);
-                    console.log('andato');
-                }
-            }, 50);
+            if (!projectsLoaded) {
+                loadAll = setInterval(function(){
+                    if (infProject.loadCount !== 2) {
+                        projectsLoaded = false;
+                        infProject.loadNextPage();
+                    } else {
+                        filterProject(filterValue);
+                        projectsLoaded = true;
+                        console.log('andato');
+                    }
+                }, 50);
+            } else {
+                filterProject(filterValue);
+            }
 
         }
 
