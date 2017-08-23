e51406d1654d2a130892cc82e53441dfe8e5f3e2
Alberto Gorgonio <gorgonioalberto@gmail.com>
2017-08-23 12:53:51 +0200

    fixed untracked files

--- app/fragments/footer.php
+++
@@ -1,4 +0,0 @@
-<footer></footer><div id="informativa_cookie"><div class="wrapper"><div id="contenitore_testo_cookie"><p>Questo sito utilizza i cookies, continuando la navigazione acconsenti all'utilizzo degli stessi.</p></div><div id="contenitore_bottone_cookie"><a id="ok_cookie" href="#">OK</a> <a href="https://it.wikipedia.org/wiki/Cookie" target="_blank" rel="noopener">Più informazioni</a></div></div></div><script>var nameCookie = '<?= str_replace(' ', '_', strtolower($siteName)) ?>_cookie';
-    var colors = <?= json_encode($colorsBkg) ?>;
-    var firstColor = '<?= $mainColor ?>';
-    var baseUrl = '<?= $baseUrl ?>';</script><script src="<?= $baseUrl ?>/js/build/production<?php if ($isDev !== 'true') echo '.min' ?>.js"></script>
